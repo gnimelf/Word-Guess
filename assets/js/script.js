f25d69f3402f573b5ef90af5b8b0e9677a830dfc
Justin <fezzygo@gmail.com>
2022-06-15 14:43:41 -0400

    added if statement to setInterval to check when count === 0 (js:53-63),added wordBank (js:7-43)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,63 @@ var currentScore = $("#current-score");
 var guessWord = $("#guess-word");
 var startBtn = $("#start-btn");
 var gameImageEl = $("#game-image");
-
-var count = 0;
+var workBank = [
+    "Algorithm", 
+    "Argument", 
+    "Arrays",
+    "Arithmetic operators",
+    "Assignment operators",
+    "Augmented reality",
+    "Autonomous",
+    "Binary numbers",
+    "Bit",
+    "C++",
+    "Camel case",
+    "Coding",
+    "Coding languages",
+    "Computer program",
+    "Conditional statements",
+    "Else statements",
+    "For loops",
+    "Functions",
+    "If statements",
+    "Integrated Development Environment",
+    "IntelliJ",
+    "Java",
+    "Jupyter Notebook",
+    "Linux",
+    "Loops",
+    "Main function",
+    "Machine learning",
+    "Neural networks",
+    "Python",
+    "Scratch",
+    "Scripts",
+    "Statement",
+    "Variable",
+    "Variable types",
+    "While loops"
+];
+var blankWord = [];
+var count = 10;
 
 function startGame(event){
+    // Select random word
+    
 
     // Timer
     var timeInterval = setInterval(()=>{
-        timerEl.text(`Timer: ${count++}`)
+
+        // Check if counter hit zero
+        if (count === 0) {
+            clearInterval(timeInterval);
+        }
+
+        timerEl.text(`Timer: ${count--}`);
+
     },1000);
+
+    gameImageEl.attr("src", )
 }
 
 startBtn.on("click", startGame);
