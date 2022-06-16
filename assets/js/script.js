3546121aeab3ef592e59d82e902d59ef9b79579e
Justin <fezzygo@gmail.com>
2022-06-16 12:47:02 -0400

    add gameOver func to stop game on win or lose (js:166-194), start resetGame func, add load highscore from localstoage func (js:188-194), add check if user got word(s) correct (js:153-157)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Selectors
 var timerEl = $("#timer");
 var highScoreEl = $("#high-score");
-var currentScore = $("#current-score");
+var currentScoreEl = $("#current-score");
 var guessWordEl = $("#guess-word");
 var startBtnEl = $("#start-btn");
 var gameImageEl = $("#game-image");
@@ -9,7 +9,7 @@ var footerCopy = $("#footer p");
 
 var year = new Date();
 
-// 
+// Variables
 var wordBank = [
     "algorithm",
     "argument",
@@ -49,34 +49,47 @@ var wordBank = [
 ];
 var blankWord = "";
 var wordToGuess = "";
-var count = 10;
+var count = 20;
 var imageNumber = 1;
 var correctGuesses = [];
+var currentScore = 0;
+var highScore = 0;
+var localStore = localStorage.getItem("wg_highscore");
+var mysteryWord = '';
+
+
+// Timer
+var timeInterval = '';
+
 
 function startGame(event) {
+    $(document).on("keyup", checkGuess);
+    
 
     getRandomWord();
 
-
     updateImage();
 
-    // Timer
-    var timeInterval = setInterval(() => {
+    timeInterval = setInterval(() => {
 
         // Check if counter hit zero
-        if (count === 0) {
+        if (count === 0 || imageNumber === 7) {
             clearInterval(timeInterval);
-            // TODO:  gameOver();
+            gameOver();
         }
-
+    
         timerEl.text(`Timer: ${count--}`);
-
+    
     }, 1000);
+
 }
 
 function updateImage() {
-    if (imageNumber < 8){
-        gameImageEl.attr("src", `./assets/images/image-${imageNumber}.png`); 
+    if (imageNumber < 8) {
+        gameImageEl.attr("src", `./assets/images/image-${imageNumber}.png`);
+    } else {
+        gameImageEl.attr("src", `./assets/images/image-${imageNumber}.png`);
+        gameOver();
     }
 }
 
@@ -84,25 +97,25 @@ function updateImage() {
 function getRandomWord() {
     var randomNum = Math.floor(Math.random() * wordBank.length)
     wordToGuess = wordBank[randomNum];
+    mysteryWord = wordToGuess;
+    mysteryWord = mysteryWord.replace(" ",' &nbsp; ')
     console.log(wordToGuess);
 
     // setup blank word
     for (var i = 0; i < wordToGuess.length; i++) {
-        if (wordToGuess[i] != ' '){
+        if (wordToGuess[i] != ' ') {
             blankWord += " _ ";
             correctGuesses.push(" _ ");
         } else {
-            console.log( );
             blankWord += ' &nbsp; ';
-            correctGuesses.push(' &nbsp; ');
+            correctGuesses.push(' ');
         }
     }
-    console.log(blankWord);
     guessWordEl.html(blankWord);
 }
 
 // TODO: When user presses button check if letter is in word and update screen
-function updateScreen(event) {
+function checkGuess(event) {
     var userGuess = (event.originalEvent.key);
     var userGuessCorrect = false;
 
@@ -115,29 +128,75 @@ function updateScreen(event) {
         if (userGuess === wordToGuess[i]) {
             correctGuesses[i] = userGuess;
             userGuessCorrect = true;
+            currentScore += 1;
+            currentScoreEl.text(`Current Score: ${currentScore}`)
         }
     }
 
     // check if user got it correct
     if (userGuessCorrect === true) {
         for (var i = 0; i < correctGuesses.length; i++) {
-            console.log(correctGuesses[i])
-            blankWord += correctGuesses[i];
-            guessWordEl.html(blankWord);
+            if (correctGuesses[i] === " "){
+                blankWord += ' &nbsp; ';
+            } else {
+                blankWord += correctGuesses[i];
+                guessWordEl.html(blankWord);
+            }
+
         }
+
     } else {
         // update image if user guess is wrong
         imageNumber += 1;
         updateImage();
     }
+    if (blankWord === mysteryWord) {
+        currentScore += count;
+        currentScoreEl.text(`Current Score: ${currentScore}`);
+        gameOver();
+    }
 }
 
 // Set copyright year in footer
 function setCopyRight() {
-    footerCopy.text(`\u00A9 Justin Fleming ${year.getFullYear()}`)
+    footerCopy.html(`&copy Justin Fleming ${year.getFullYear()}`)
 }
 
+
+function gameOver() {
+    clearInterval(timeInterval);
+    if (highScore < currentScore) {
+        highScore = currentScore;
+        localStorage.setItem("wg_highscore", highScore);
+    } else {
+        guessWordEl.html(wordToGuess)
+    }
+    var playAgain = confirm("Game Over! Would you like to play again?");
+    if (playAgain === true){
+        resetgame();
+        startGame();
+    } else {
+        resetgame();
+    }
+}
+
+// reset game board
+function resetgame() {
+
+}
+
+function loadHighScore() {
+    highScore = localStore;
+    if (highScore == null) {
+        highScore = 0;
+        localStorage.setItem("wg_highscore", highScore);
+    }
+    highScoreEl.text(`High Score: ${highScore}`);
+}
+
+loadHighScore();
 setCopyRight();
 
+// Event listeners
 startBtnEl.on("click", startGame);
-$(document).on("keyup", updateScreen);
+
